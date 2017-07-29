322b8740865a752decad0c0d54a7c9be7bbcc3a3
Michael Greenberg <mgreenberg@tonicdesign.com>
2017-07-28 22:07:05 -0400

    Fetch beer data to be displayed in the beer container

--- app/scripts/homePage.js
+++ app/scripts/homePage.js
@@ -1,46 +1,83 @@
+var _ = require('lodash');
 import { BreweryDB } from 'scripts/breweryDB.js'
 
 class HomePage {
   constructor() {
+    this.beerData = null;
+    this.bindEvents();
     this.breweryDB = new BreweryDB();
-    // this.breweryDB.fetchStyles()
-    //   .then((data) => { this.populateBeerStyles(data); })
-    //   .catch((error) => { this.populateBeerStylesFailed(error); });
-    this.breweryDB.fetchCategories()
-      .then((data) => { this.populateBeerCategories(data); })
-      .catch((error) => { this.populateBeerStylesFailed(error); });
+    this.breweryDB.fetchStyles()
+      .then((data) => { this.populateBeerData(data); })
+      .catch((error) => { this.populateBeerDataFailed(error);
+    });
   }
 
-  populateBeerCategories(data){
-    console.log(data)
-    for (var i=0; i < data.data.length; i++) {
-      var typesOfBeer = [
-        '<div class="twelve columns">',
-          '<p class="name">' + data.data[i].name + '</p>',
-        '</div>'
-      ].join('');
-      $("#beer-list").append(typesOfBeer);
-    }
+  populateBeerData(data) {
+    this.beerData = data;
+    this.populateQuestionOne();
   }
 
-  populateBeerStyles(data) {
-    for (var i=0; i < data.data.length; i++) {
-      var typesOfBeer = [
-        '<div class="twelve columns">',
-          '<p class="name">' + data.data[i].name + '</p>',
-        '</div>'
-      ].join('');
-      $("#beer-list").append(typesOfBeer);
-    }
-  }
-
-  populateBeerStylesFailed() {
+  populateBeerDataFailed() {
     var errorMessage  = [
         '<div class="twelve columns text-center">',
           '<h1>Oops Something Went Wrong</h1>',
         '</div>'
       ].join('');
     $("#beer-list").append(errorMessage);
+  }
+
+  populateQuestionOne() {
+    var questionOne = $('.question-one');
+    var uniqueBeerCategories = _.uniqBy(this.beerData.data, 'categoryId');
+    this.appendPlaceholderOption(questionOne);
+    _.forEach(uniqueBeerCategories, function(beerCategory) {
+      var categoryOption = '<option value="' + beerCategory.categoryId + '">'+ beerCategory.category.name + '</option>'
+      questionOne.append(categoryOption);
+    })
+  }
+
+  appendPlaceholderOption(select){
+    select.append('<option disabled selected value> -- select an option -- </option>');
+  }
+
+  resetSelectOptions(select) {
+    select.find('option').remove();
+  }
+
+  handleQuestionOneAction() {
+    var questionTwo = $('.question-two');
+    var selectedCategory = $('.question-one').val();
+    this.resetSelectOptions(questionTwo);
+    this.appendPlaceholderOption(questionTwo);
+    _.forEach(this.beerData.data, function(categoryStyle) {
+      if(categoryStyle.categoryId == selectedCategory) {
+        var categoryStyleOption = '<option value="' + categoryStyle.id + '">'+ categoryStyle.name + '</option>'
+        questionTwo.append(categoryStyleOption);
+      }
+    })
+    $('.question-two-container').show();
+  }
+
+  handleQuestionTwoAction() {
+    var selectedCategoryStyle = $('.question-two').val();
+    var paramaters = '&styleId='+ selectedCategoryStyle + '&p=1&sort=ASC'
+    this.breweryDB.fetchBeers(paramaters)
+      .then((data) => { this.populateBeerContainer(data); })
+      .catch((error) => { this.populateBeerDataFailed(error);
+    });
+    $('.beer-card-container').show();
+  }
+
+  populateBeerContainer(data) {
+
+
+    debugger
+
+  }
+
+  bindEvents() {
+    $('.question-one').on('change', this.handleQuestionOneAction.bind(this));
+    $('.question-two').on('change', this.handleQuestionTwoAction.bind(this));
   }
 }
 
